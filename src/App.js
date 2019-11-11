826af10024d02dc246377426b123c89ba60f48e9
Randi Øyri <roy@difi.no>
2019-11-11 12:41:28 +0100

    layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component, Fragment} from 'react';
 import './App.css';
 
-function App() {
+class App extends Component{
+
+render() {
+
+  const authText = "NOT authenticated";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+
+          You are: {authText}
+
+        </header>
+      </div>
   );
+}
 }
 
 export default App;
