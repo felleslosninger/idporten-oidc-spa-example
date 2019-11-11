ab3699699cf6fb86331ad745110c2eb788e340b4
Randi Øyri <roy@difi.no>
2019-11-11 15:26:11 +0100

    logout from idporten with correct urls

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ const manager = new UserManager({
     authority: "https://oidc-ver2.difi.no/idporten-oidc-provider",
     client_id: "713848d2-288c-45a3-ae33-3d8e1cd00de2",
     redirect_uri: "http://localhost:3000",
+    silent_redirect_uri: "http://localhost:3000",
+    post_logout_redirect_uri: "http://localhost:3000",
     response_type: "code",
     scope: "openid profile",
+    loadUserInfo: false
 });
 
 class App extends Component {
