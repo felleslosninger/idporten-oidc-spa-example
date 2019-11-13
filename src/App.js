c89b372ebe9801d6469f5ba1328470a191556f04
Randi Øyri <roy@difi.no>
2019-11-13 14:08:27 +0100

    call eventlog-api from systest - > failing parsing

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import {UserManager} from 'oidc-client';
 
 const manager = new UserManager({
-    authority: "https://oidc-ver2.difi.no/idporten-oidc-provider",
-    client_id: "713848d2-288c-45a3-ae33-3d8e1cd00de2",
-    redirect_uri: "http://localhost:3000",
-    silent_redirect_uri: "http://localhost:3000",
-    post_logout_redirect_uri: "http://localhost:3000",
+   // authority: "https://oidc-ver2.difi.no/idporten-oidc-provider",
+   // client_id: "713848d2-288c-45a3-ae33-3d8e1cd00de2",
+    authority: "https://eid-systest-web01.dmz.local/idporten-oidc-provider",
+    client_id: "oidc_idporten_brukerprofil_oidc",
+    redirect_uri: "http://localhost:3000/",
+    silent_redirect_uri: "http://localhost:3000/",
+    post_logout_redirect_uri: "http://localhost:3000/",
     response_type: "code",
-    scope: "openid profile",
+    //scope: "openid profile idporten:authorizations.read",
+    scope: "openid profile idporten:authorizations.admin idporten:user.log.all.read",
     loadUserInfo: false
 });
 
@@ -24,7 +27,7 @@ class App extends Component {
         this.state = {
             user: null,
             apihealth: null,
-            userdata: null
+            userdata: []
         };
     }
 
@@ -55,14 +58,28 @@ class App extends Component {
     }
 
     callOppslagstjenestenApi(e) {
+        //fetch("https://eid-systest-web01.dmz.local/authorizations",{
+        fetch("https://eid-systest-web01.dmz.local/eventlog-api/logg/idporten/all?maxhits=1",{
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + this.state.user.access_token,
+            }
+        })
+        //fetch("https://api.idporten-ver2.difi.no/authorizations")
+        //fetch("https://api.idporten-ver2.difi.no/innlogginger/logg/idporten/all?maxhits=1")
+            .then(res => res.json())
+            .then((result) => this.setState({ userdata: result }))
+            .catch(function(error) {
+                console.log(error);
+            });
     }
 
     callApiHealth(e) {
-        let currentComponent = this;
         fetch("https://api.idporten-ver2.difi.no/innlogginger/health")
         //fetch("https://oidc-ver2.difi.no/kontaktinfo-oauth2-server/health")
-            .then(res => res.json())
-            .then((result) => currentComponent.setState({ apihealth: result.status }))
+            .then(resp => resp.json())
+            .then((health) => this.setState({ apihealth: health.status }))
             .catch(function(error) {
             console.log(error);
         });
@@ -71,8 +88,9 @@ class App extends Component {
     render() {
 
         const authText = this.isAuthenticated() ? "authenticated" : "NOT authenticated";
-        const { user, apihealth, userdata } = this.state;
-
+        const { user, apihealth } = this.state;
+        const { userdata } = this.state.userdata;
+        console.log(userdata);
         return (
             <div className="App">
                 <header className="App-header">
@@ -98,17 +116,26 @@ class App extends Component {
                     <button onClick={this.loginComplete}>Complete login</button>
                     <br/>
                     {apihealth &&
-                    <p>API is: {apihealth}</p>
+                    <Fragment><p>API is: {apihealth}</p></Fragment>
 
                     }
                     <button onClick={this.callApiHealth}>Call API</button>
                     <br/>
 
-                    {userdata &&
-                    <p>API has this data about you: {userdata}</p>
 
+                    {userdata && userdata.length && <Fragment><p>API has this data about you:</p>
+                        userdata.map((item, index) => {
+                             <div> Innlogging med ID-porten:
+
+                            </div>
+                        }
+                        );
+                    </Fragment>
                     }
+
+
                     <button onClick={this.callOppslagstjenestenApi}>Fetch data from API</button>
+
                     <br/>
                     <button onClick={this.logoutStart}>Start logout</button>
                     <button onClick={this.logoutComplete}>Complete logout</button>
